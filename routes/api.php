607dc1abdc4dcc9605542b207d9b6970836d8f47
Kim Palao <kimfarhantpalao@gmail.com>
2021-12-13 11:53:00 +0800

    Add bag movement model, migration, seeder, and factory

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Bag;
+use App\Models\BagImage;
+use App\Models\BagMovement;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +22,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::get('/inventory', function (Request $request) {
     return ['data' => Bag::all()];
+});
+Route::get('/bag/image/{id}/{index}', function ($id, $index) {
+    $image = BagImage::whereBagId($id)->limit(1)->offset($index)->first();
+    return response()->file(public_path($image->file_name));
+});
+Route::get('/bag/movement/{id}', function ($id) {
+    return ['data' => BagMovement::whereBagId($id)->orderBy('datetime')->get()->all()];
 });
