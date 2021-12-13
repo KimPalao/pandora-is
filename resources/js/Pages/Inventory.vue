f89b7f159d40f306a1ee4c26af2cb507053459b5
Kim Palao <kimfarhantpalao@gmail.com>
2021-12-13 11:44:03 +0800

    Show bags in inventory page

--- resources/js/Pages/Inventory.vue
+++ resources/js/Pages/Inventory.vue
@@ -9,7 +9,14 @@
         <div class="col-md-12">
           <div class="card shadow bg-light">
             <div class="card-body bg-white px-5 py-3 border-bottom rounded-top">
-              <div class="mx-3 my-3">HI</div>
+              <DataTable :value="bags" responsiveLayout="scroll">
+                <Column
+                  v-for="col of columns"
+                  :field="col.field"
+                  :header="col.header"
+                  :key="col.field"
+                ></Column>
+              </DataTable>
             </div>
           </div>
         </div>
@@ -21,11 +28,34 @@
 <script>
 import { defineComponent } from "vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
-import Welcome from "@/Jetstream/Welcome.vue";
+import axios from "axios";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
 
 export default defineComponent({
   components: {
     AppLayout,
+    DataTable,
+    Column,
+  },
+  data() {
+    return {
+      bags: [],
+      columns: [
+        {
+          field: "name",
+          header: "Name",
+        },
+        {
+          field: "price",
+          header: "Price",
+        },
+      ],
+    };
+  },
+  async mounted() {
+    const response = await axios.get("/api/inventory");
+    this.bags = response.data.data;
   },
 });
 </script>
