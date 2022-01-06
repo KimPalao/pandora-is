37afcf97658fd6370dfaa43c0434e6c820b12478
Kim Palao <kimfarhantpalao@gmail.com>
2022-01-07 01:53:47 +0800

    Support multiple items in query

--- resources/js/Pages/Query.vue
+++ resources/js/Pages/Query.vue
@@ -9,27 +9,42 @@
         <div class="col-md-12">
           <div class="card shadow bg-light">
             <div class="card-body bg-white px-5 py-3 border-bottom rounded-top">
-              <h5 class="card-title">What do you want to make?</h5>
               <div class="row">
-                <div class="col-6">
+                <div class="col">
+                  <h5 class="card-title">What do you want to make?</h5>
+                </div>
+                <div class="col-auto">
+                  <Button @click="increment_products" class="me-2"
+                    >+ Add Product</Button
+                  >
+                  <Button @click="query">Query</Button>
+                </div>
+              </div>
+              <div
+                class="row mb-4"
+                v-for="(selected, index) in selection"
+                :key="`product-${index}`"
+              >
+                <div class="col-1">
+                  <Button @click="remove(index)" v-if="index !== 0">x</Button>
+                </div>
+                <div class="col-5">
                   <Dropdown
-                    v-model="selected_product"
-                    :options="products"
+                    v-model="selected.product"
+                    :options="selected.products"
                     option-label="name"
                     option-value="id"
                     placeholder="Select a Product"
                     filter
-                    @filter="filter_products"
-                    :loading="loading_products"
+                    @filter="filter_products($event, index)"
+                    :loading="selected.loading"
                     class="w-100"
                   />
                 </div>
                 <div class="col-3">
-                  <InputNumber v-model="quantity" class="w-100" />
+                  <InputNumber v-model="selected.quantity" class="w-100" />
                 </div>
-                <div class="col-2">
-                  <Button @click="query">Query</Button>
-                </div>
+                <div class="col-2"></div>
               </div>
 
               <h5 class="card-title mt-5">
@@ -95,41 +110,53 @@ export default defineComponent({
   },
   data() {
     return {
-      products: [],
-      selected_product: 0,
-      quantity: 0,
-      loading_products: false,
+      selection: [
+        {
+          product: 0,
+          products: [],
+          loading: false,
+          quantity: 0,
+        },
+      ],
 
       resources: [],
       query_loading: false,
     };
   },
   methods: {
-    async get_data() {
-      const [sales] = await Promise.all([axios.get("/api/sales/recent")]);
-      this.sales = sales.data.data;
+    increment_products() {
+      this.selection.push({});
     },
-    async filter_products(event) {
-      this.products_loading = true;
+    async filter_products(event, index) {
+      this.selection[index].loading = true;
       try {
         const response = await axios.get("/api/products", {
           params: {
             name: event.value,
           },
         });
-        this.products = response.data.data;
+        this.selection[index].products = response.data.data;
       } catch (e) {
       } finally {
-        this.products_loading = false;
+        this.selection[index].loading = false;
       }
+    },
+    remove(index) {
+      this.selection.splice(index, 1);
     },
     async query() {
       this.query_loading = true;
       try {
+        const products = [];
+        const quantities = [];
+        for (let selection of this.selection) {
+          products.push(selection.product);
+          quantities.push(selection.quantity);
+        }
         const response = await axios.get("/api/resolve-products", {
           params: {
-            products: this.selected_product,
-            quantities: this.quantity,
+            products: products.join(","),
+            quantities: quantities.join(","),
           },
         });
         this.resources = response.data.resources;
@@ -139,9 +166,6 @@ export default defineComponent({
         this.query_loading = false;
       }
     },
-  },
-  mounted() {
-    this.get_data();
   },
 });
 </script>
