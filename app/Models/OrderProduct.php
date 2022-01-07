11ac74b9499455844fd21f55b2632cf80353290f
Kim Palao <kimfarhantpalao@gmail.com>
2022-01-07 21:30:29 +0800

    Add Order and OrderProduct models

---
+++ app/Models/OrderProduct.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\OrderProduct
+ *
+ * @property int $id
+ * @property int $order_id
+ * @property int $product_id
+ * @property int $quantity
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderProduct newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderProduct newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderProduct query()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderProduct whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderProduct whereOrderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderProduct whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderProduct whereQuantity($value)
+ * @mixin \Eloquent
+ */
+class OrderProduct extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_01_07_211839_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('total', 19);
+            $table->timestamps();
+        });
+        Schema::create('order_products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('quantity');
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_products');
+        Schema::dropIfExists('orders');
+    }
+}
